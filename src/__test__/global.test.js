fa3a0ae854927cb2b58ec9f1b316aefbe9f31a03
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2020-10-20 14:07:13 -0500

    Implementando pruebas para Boolean y Array

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,6 +1,34 @@
+//Probar string
 const text = "Hola mundo";
 
 test('Debe contener un texto', () => {
     expect(text).toMatch(/mundo/);
 });
 
+//Probar array
+const fruits = ['manzana', 'melon', 'banana'];
+
+test('Tenemos una banana?', () => {
+    expect(fruits).toContain('banana');
+});
+
+//Probar números
+test('Mayor que', () => {
+    expect(10).toBeGreaterThan(9);
+});
+
+//Probar un Booleano
+test('Verdadero', () => {
+    expect(true).toBeTruthy();
+});
+
+//Probar callback
+const reverseString = (str, callback) => {
+    callback(str.split("").reverse().join(""))
+};
+
+test('Probar un Callback', () => {
+    reverseString('Hola', (str) => {
+        expect(str).toBe('aloH');
+    })
+});
